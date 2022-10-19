680c25ddffdaa54d21a2e841d164840f631ab7d5
Cindi Nichols <cindi.l.nichols@gmail.com>
2022-10-19 11:46:22 -0600

    todo

--- ToDo/Todos.js
+++ ToDo/Todos.js
@@ -1,40 +1,69 @@
 import * as ls from './ls.js';
 import * as util from './utilities.js';
 
-let todoList = [];
+let todoList = null;
 
 /*in the constructor you should set a variable with the element our todo list will be built in, 
 and the key we will use to read/write from localStorage*/
 export default class Todos {
-   constructor (element, key) {
+   constructor(element, key) {
       this.element = util.qs(element);
       this.key = key;
+      this.addEventListeners();
    }
 
    // Add a method to the Todos class called addTodo. It should grab the input in the html where users enter the text of the task, 
-      // then send that along with the key to a SaveTodo() function. Then update the display with the current list of tasks
+   // then send that along with the key to a SaveTodo() function. Then update the display with the current list of tasks
    addTodo() {
       const task = util.qs('#task').value;
-      saveTodo(key, task);
+      saveTodo(this.key, task);
       this.listTodos();
    }
 
    listTodos() {
       // Add a method to the Todos class called listTodos(). It should use the renderTodoList function to output our todo list when called.
       // It should get called when a todo is added, or removed, and when the Todos class is instantiated.
-      const list = getTodos(this.key);
-      renderTodoList(list, this.element);
+      todoList = getTodos(this.key);
+      updateTasksLeft(todoList);
+      renderTodoList(todoList, this.element);
    }
 
-   completeTodo() {
+   completeTodo(e) {
+      if (!e.target.matches("input[type='checkbox']")) return;
+      const todo = todoList.find(todo => todo.id === e.target.parentElement.id);
+      todo.completed = !todo.completed;
+      ls.writeToLS(this.key, todoList);
+      updateTasksLeft(todoList);
+   }
+
+   removeTodo(e) {
+      if (e.target && e.target.matches("button.delete-button")) {
+         todoList.splice(todoList.findIndex(i => i.id === e.target.parentElement.id), 1);
+         ls.writeToLS(this.key, todoList);
+         this.listTodos();
+      }
 
    }
 
-   removeTodo() {
-
+   filterTodo(e) {
+      if (e.target && e.target.matches("button.active-button")) {
+         const activeTasks = todoList.filter(task => task.completed === false);
+         renderTodoList(activeTasks, this.element);
+      } else if (e.target && e.target.matches("button.completed-button")) {
+         const completedTasks = todoList.filter(task => task.completed === true);
+         renderTodoList(completedTasks, this.element);
+      } else {
+         renderTodoList(todoList, this.element);
+      }
    }
 
-   filterTodo() {
+   addEventListeners() {
+      const addTask = util.qs('#add-task');
+      util.onTouch(addTask, this.addTodo.bind(this));
+      util.onTouch(this.element, this.removeTodo.bind(this));
+      util.onTouch(this.element, this.completeTodo.bind(this));
+      const filterTask = util.qs('.list-footer');
+      util.onTouch(filterTask, this.filterTodo.bind(this));
 
    }
 
@@ -48,12 +77,10 @@ export default class Todos {
 * @param {string} task The text of the task to be saved. 
 
  */
-function saveTodo(key, task) { 
+function saveTodo(key, task) {
    const timestamp = new Date();
-   const todo = {id: timestamp, content: task, completed: "false"};
-   todoList = getTodos(key);
+   const todo = { id: timestamp, content: task, completed: false };
    todoList.push(todo);
-   console.log(todoList);
    ls.writeToLS(key, todoList);
 }
 
@@ -66,11 +93,12 @@ If it is null then pull the list of todos from localstorage, update the local va
 @param {string} key The key under which the value is stored under in LS 
 @return {array} The value as an array of objects
 */
-function getTodos(key) { 
-   if (todoList === undefined || todoList === null || todoList.length === 0) {
+function getTodos(key) {
+   if (todoList === null) {
       todoList = ls.readFromLS(key);
       return todoList;
    }
+   return todoList;
 }
 
 
@@ -79,16 +107,25 @@ function getTodos(key) {
 @param {element} element The DOM element to insert our list elements into.
 
 */
-function renderTodoList(list, element) { 
-   
+function renderTodoList(list, element) {
+   element.innerHTML = '';
    list.forEach(task => {
       const taskLi = document.createElement('li');
-      taskLi.innerHTML = `<input type="checkbox" />
-                        <label for="${task.id}">${task.content}</label>
+      taskLi.setAttribute("id", `${task.id}`);
+      taskLi.innerHTML = `<input id="${task.id} "type="checkbox" ${task.completed ? 'checked' : ''}/>
+                        <label for="${task.id}" class="task-content">${task.content}</label>
                         <button type="submit" class="delete-button">X</button>`
       element.append(taskLi);
    });
 }
+
+
+function updateTasksLeft(list) {
+   const taskLeft = list.filter(task => task.completed === false).length;
+   const p = util.qs("#num-left");
+   p.innerHTML = `${taskLeft} task${taskLeft === 1 ? '' : 's'} left`;
+}
+
 
 
 
--- ToDo/main.js
+++ ToDo/main.js
@@ -5,12 +5,3 @@ const todos = new Todos("#task-list", "task");
 
 todos.listTodos();
 
-const addTask = util.qs('#add-task');
-util.onTouch(addTask, todos.addTodo);
-
-
-
-console.log (todos.key);
-console.log (todos.element);
-
-
